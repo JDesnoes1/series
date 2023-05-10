b42d5c43b366310dec46933101950a77483fd804
Jérémie Desnoes <j.desnoes1@gmail.com>
2023-05-10 14:27:31 +0200

    Controller pour serie terminado

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -6,24 +6,25 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/series', name: 'serie')]
+#[Route('/series', name: 'serie_')]
 class SerieController extends AbstractController
 {
-    #[Route('/', name: 'serie_list')]
+    #[Route('/', name: 'list')]
     public function list(): Response
     {
         //TODO renvoyer la liste des series
         return $this->render('serie/list.html.twig');
     }
 
-    #[Route('/{id}', name: 'serie_show')]
+    #[Route('/{id}', name: 'show',  requirements: ["id" => "\d+"])]
     public function show(int $id): Response
     {
+        dump($id);
         //TODO renvoyer le détail d'une série
         return $this->render('serie/show.html.twig');
     }
 
-    #[Route('/add', name: 'serie_add')]
+    #[Route('/add', name: 'add')]
     public function add(): Response
     {
         //TODO renvoyer un formulaire pour créer une nouvelle série
